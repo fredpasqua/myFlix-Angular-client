46e0e34faef26593bc085be20091aab99e6638f5
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-08-07 13:06:19 -0400

    Added router to Login Component

--- src/app/user-login-form/user-login-form.component.ts
+++ src/app/user-login-form/user-login-form.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
 //close dialogue on success
 import { MatDialogRef } from '@angular/material/dialog';
-
+import { Router } from '@angular/router';
 //This Import to bring in the API calls
 import { FetchApiDataService } from '../fetch-api-data.service';
 
 //Import is used to display notifications back to the user
 import { MatSnackBar } from '@angular/material/snack-bar';
 
+
+
+
 @Component({
   selector: 'app-user-login-form',
   templateUrl: './user-login-form.component.html',
   styleUrls: ['./user-login-form.component.scss']
 })
+
 export class UserLoginFormComponent implements OnInit {
-  
+ 
   @Input() userData = {Username: '', Password: ''};
   constructor(
       public fetchApiData: FetchApiDataService, 
       public dialogRef: MatDialogRef<UserLoginFormComponent>,
-      public snackBar: MatSnackBar) { }
-
+      public snackBar: MatSnackBar,
+      public router: Router
+  ) {}
   ngOnInit(): void {
   }
 
@@ -29,6 +34,7 @@ export class UserLoginFormComponent implements OnInit {
       // Add token and username to local Storage
       localStorage.setItem('token', response.token);
       localStorage.setItem('user', response.user.Username);
+      this.router.navigate(['movies']);
       this.dialogRef.close(); //This will close the modal on success!
       this.snackBar.open('User login successful', 'OK', {
         duration: 2000
