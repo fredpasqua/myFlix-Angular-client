6535da82b13d8edfb3d0733aed768c0f0618ee71
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2022-08-09 00:44:46 -0400

    adding buttons for favorite movies

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -9,12 +9,17 @@ import { FetchApiDataService } from '../fetch-api-data.service';
 export class MovieCardComponent {
   movies: any[] = [];
   loading: boolean = false;
-
+  favorites: any[] = ["621837d7a9f7d842a1f68dad"];
   constructor(public fetchApiData: FetchApiDataService) { }
 
   ngOnInit(): void {
     this.loading = true;
     this.getMovies();
+    this.getFavorites();
+  }
+
+  isFav(id: string): boolean {
+    return this.favorites.includes(id)
   }
 
   getMovies(): void {
@@ -25,4 +30,21 @@ export class MovieCardComponent {
       return this.movies;
     });
   }
+
+  getFavorites(): void {
+    const user = localStorage.getItem('user');
+    this.fetchApiData.getUser(user).subscribe((resp: any) => {
+      const userFavorites = (resp._id);
+      console.log(resp._id)
+      this.favorites.push(userFavorites);
+    })
+  }
+
+  addFavorite(_id: string): void {
+    this.fetchApiData.addFavoriteMovie(_id).subscribe((result) => {
+      console.log(result);
+      this.ngOnInit();
+    })
+  };
+ 
 }
